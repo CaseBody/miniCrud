58114f7463cc80328a4d267515e76fd5f0d12c2f
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-03-14 13:14:09 +0100

    Change titles and styling on homepage

---
+++ index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="styles.css" />
+		<title>Cafetaria Cicek</title>
+
+		<link rel="preconnect" href="https://fonts.googleapis.com" />
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+		<link
+			href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500&family=Roboto+Mono:wght@100;400;700&display=swap"
+			rel="stylesheet"
+		/>
+	</head>
+	<body class="home">
+		<main>
+			<header>
+				<img class="logo-header" src="images/logo.png" alt="" />
+				<div class="links">
+					<ul>
+						<li>
+							<a href="">Home</a>
+						</li>
+						<li>
+							<a href="">Bestellen</a>
+						</li>
+					</ul>
+					<a href=""> <img class="winkel-karretje" src="images/karretje.png" alt="" /></a>
+				</div>
+			</header>
+			<div class="content">
+				<p class="titel">Welkom bij cafetaria Cicek in Nijmegen</p>
+			</div>
+			<!-- <footer>
+				<ul>
+					<li><a href="">Telefoon nummer</a></li>
+					<li><p>-</p></li>
+					<li><a href="">Email</a></li>
+					<li><p>-</p></li>
+					<li><a href="">Adres</a></li>
+				</ul>
+			</footer> -->
+		</main>
+		<div class="info-blokken">
+			<div class="blok">
+				<p class="titel">OPENINGSTIJDEN</p>
+				<p class="desc">Op deze tijden zijn wij geopened</p>
+
+				<table>
+					<tr>
+						<th>Afhalen</th>
+						<th>Bezorgen</th>
+					</tr>
+					<tr>
+						<td>15:00 - 23:59</td>
+						<td>16:00 - 23:45</td>
+					</tr>
+				</table>
+			</div>
+
+			<div class="blok">
+				<p class="titel">BESTELLEN</p>
+				<p class="desc">Bestel nu uw favorite gerechten.</p>
+
+				<a class="blok-link" href="">Bestel ></a>
+			</div>
+
+			<div class="blok">
+				<p class="titel">SOLLICITEREN</p>
+				<p class="desc">Zoek je een leuke baan? Kom ons dan versterken.</p>
+
+				<a class="blok-link" href="">Solliciteer ></a>
+			</div>
+		</div>
+	</body>
+</html>
