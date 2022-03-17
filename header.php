87b19ff0cc6ada8d75a1523aa5636ed338fa5fbe
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-03-17 10:22:03 +0100

    Header

---
+++ header.php
@@ -0,0 +1,14 @@
+<header>
+	<img class="logo-header" src="images/logo.png" alt="" />
+	<div class="links">
+		<ul>
+			<li>
+				<a href="">Home</a>
+			</li>
+			<li>
+				<a href="">Bestellen</a>
+			</li>
+		</ul>
+		<a href=""> <img class="winkel-karretje" src="images/karretje.png" alt="" /></a>
+	</div>
+</header>
--- index.php
+++ index.php
@@ -16,20 +16,9 @@
 	</head>
 	<body class="home">
 		<main>
-			<header>
-				<img class="logo-header" src="images/logo.png" alt="" />
-				<div class="links">
-					<ul>
-						<li>
-							<a href="">Home</a>
-						</li>
-						<li>
-							<a href="">Bestellen</a>
-						</li>
-					</ul>
-					<a href=""> <img class="winkel-karretje" src="images/karretje.png" alt="" /></a>
-				</div>
-			</header>
+			<?php 	
+				include_once("header.php");
+			?>
 			<div class="content">
 				<p class="titel">Welkom bij cafetaria Cicek in Nijmegen</p>
 			</div>
@@ -43,6 +32,7 @@
 				</ul>
 			</footer> -->
 		</main>
+
 		<div class="info-blokken">
 			<div class="blok">
 				<p class="titel">OPENINGSTIJDEN</p>
@@ -64,14 +54,142 @@
 				<p class="titel">BESTELLEN</p>
 				<p class="desc">Bestel nu uw favorite gerechten.</p>
 
-				<a class="blok-link" href="">Bestel ></a>
+				<a class="blok-link" href="">Bestel</a>
 			</div>
 
 			<div class="blok">
 				<p class="titel">SOLLICITEREN</p>
 				<p class="desc">Zoek je een leuke baan? Kom ons dan versterken.</p>
 
-				<a class="blok-link" href="">Solliciteer ></a>
+				<a class="blok-link" href="">Solliciteer</a>
+			</div>
+		</div>
+
+		<div class="menu">
+			<div class="categories">
+				<ul>
+					<li>
+						<button class="selected_catagorie" type="button">Vleesgerechten</button>
+					</li>
+					<li>
+						<button type="button">Pizza's</button>
+					</li>
+					<li>
+						<button type="button">Kapsalon's</button>
+					</li>
+					<li>
+						<button type="button">Turkse Pizza's</button>
+					</li>
+					<li>
+						<button type="button">Broodjes</button>
+					</li>
+					<li>
+						<button type="button">Bijgerechten</button>
+					</li>
+					<li>
+						<button type="button">Friet</button>
+					</li>
+					<li>
+						<button type="button">Snacks</button>
+					</li>
+					<li>
+						<button type="button">Sauzen</button>
+					</li>
+					<li>
+						<button type="button">Dranken</button>
+					</li>
+				</ul>
+			</div>
+			<div class="items">
+				<div class="item">
+					<p class="titel">Shoarma Friet</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 7.50</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Shoarma Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 10.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Lamsshoarma Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 10.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Doner Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 10.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Kipfilet Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 11.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Fantasie Shoarma Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 10.50</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Schnitzel Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 9.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Hamburger Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 9.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Mix Schotel</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 15.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
+
+				<div class="item">
+					<p class="titel">Doner Schotel Fantasie</p>
+					<img src="https://overetengesproken.nl/blog/wp-content/uploads/2019/08/shoarma-van-oesterzwam-1.jpg" alt="" />
+					<div class="bottom">
+						<p class="prijs">€ 11.00</p>
+						<button type="button">Bestel</button>
+					</div>
+				</div>
 			</div>
 		</div>
 	</body>
