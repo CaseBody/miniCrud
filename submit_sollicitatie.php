8529e28a59df67be4e94358adfb94042736a910f
CaseBody <42807313+CaseBody@users.noreply.github.com>
2022-03-24 11:09:22 +0100

    js voor de solliciteer pagina

---
+++ submit_sollicitatie.php
@@ -0,0 +1,28 @@
+<?php
+ 
+/*
+* Write your logic to manage the data
+* like storing data in database
+*/
+ 
+require_once("includes/connect.php");
+/**
+* @var PDO $connect
+*/
+
+$sql = 'INSERT INTO MENU (naam, geboorte_datum, email, telefoonnummer, toelichting)
+VALUES (":naam, :geboorte_datum, email, telefoonnummer, toelichting")';
+$stmt = $connect->prepare($sql);
+$stmt->bindParam(":naam", $_POST["naam"]);
+$stmt->bindParam(":geboorte_datum", $_POST["geboorte_datum"]);
+$stmt->bindParam(":email", $_POST["email"]);
+$stmt->bindParam(":telefoonnummer", $_POST["telefoonnummer"]);
+$stmt->bindParam(":toelichting", $_POST["toelichting"]);
+$stmt->execute();
+$result = $stmt->fetchAll();
+ 
+echo json_encode($result);
+
+exit;
+ 
+?>
